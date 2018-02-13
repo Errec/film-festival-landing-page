e9559f170e1dadc89016b591f65e39b3807c54ff
Raniro Coelho <errecme@gmail.com>
2018-02-12 22:32:33 -0200

    Add navbar

--- gulpfile.js
+++ gulpfile.js
@@ -166,7 +166,7 @@ gulp.task('setup', function() {
 
 gulp.task('watch', function() {
   gulp.watch('styles/**/*',                        ['styles']);
-  gulp.watch(['templates/**/*', './*.pug'],        ['templates']);
+  gulp.watch(['templates/**/*.pug', './*.pug'],        ['templates']);
   gulp.watch('js/*.js',                            ['scripts']);
   gulp.watch('img/**/*',                           ['images']);
 
