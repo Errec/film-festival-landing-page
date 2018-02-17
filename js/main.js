7d51c3a6b9e48d67b0c41e346b92cb052e51ade2
Raniro Coelho <errecme@gmail.com>
2018-02-17 17:43:28 -0200

    Adjust card size and slick configs

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
   $('.movies__carousel').slick({
-    dots: false,
+    arrows: true,
     centerMode: true,
     infinite: true,
-    // slidesToScroll: 4,
-    // speed: 500,
-    // variableWidth: false,
+    slidesToScroll: 4,
+    speed: 400,
+    variableWidth: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: false
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true
         }
       },
       {
