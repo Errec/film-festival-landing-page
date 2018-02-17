9351a6e67ccbb20b90ea9e94e17143c80d040986
Raniro Coelho <errecme@gmail.com>
2018-02-17 11:48:00 -0200

    Set movies slider style

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
 $(document).ready(function(){
   $('.movies__carousel').slick({
-    dots: true,
+    dots: false,
+    centerMode: true,
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
+    // slidesToScroll: 4,
+    // speed: 500,
+    // variableWidth: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          arrows: false,
+          infinite: true,
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          infinite: true,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   });
 });
+
