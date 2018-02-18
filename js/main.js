ba24ba83268f8c87336f4b890cd371db15a89d84
Raniro Coelho <errecme@gmail.com>
2018-02-17 22:33:58 -0200

    Update scroll functions: add hero btn

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 var scroolToSections = (function() {
   var moviesLink  = document.querySelector('.header__link-movies');
   var aboutLink   = document.querySelector('.header__link-about');
+  var heroBtn   = document.querySelector('.hero__btn');
   var contactLink = document.querySelector('.header__link-contact');
 
   var moviesDiv  = document.querySelector('.movies');
   var aboutDiv   = document.querySelector('.infos-register');
+  var registerDiv   = document.querySelector('.register');
   var contactDiv = document.querySelector('.contact');
 
   moviesLink.addEventListener('click', _scrollIntoMovies);
   aboutLink.addEventListener('click', _scrollIntoAbout);
+  heroBtn.addEventListener('click', _scrollIntoRegister);
   contactLink.addEventListener('click', _scrollIntoContact);
 
   function _scrollIntoMovies(e) {
@@ -19,6 +22,12 @@ var scroolToSections = (function() {
     e.preventDefault();
     aboutDiv.scrollIntoView({ behavior: 'smooth' });
   }
+
+  function _scrollIntoRegister(e) {
+    e.preventDefault();
+    registerDiv.scrollIntoView({ behavior: 'smooth' });
+  }
+
   function _scrollIntoContact(e) {
     e.preventDefault();
     contactDiv.scrollIntoView({ behavior: 'smooth' });
